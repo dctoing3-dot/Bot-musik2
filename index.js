cb1bad3c56dffd17c015130709d21850a7892ae2
dctoing3-dot <dctoing3@gmail.com>
2026-02-06 04:49:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ const client = new Client({
 });
 
 // ============ LAVALINK NODES ============
-const Nodes = [
+const Nodes = [ 
     {
-        name: 'Main',
-        url: 'lavalinkv4.serenetia.com:443',
-        auth: 'https://dsc.gg/ajidevserver',
+        name: 'MyRender',
+        url: 'lavalink-sf9r.onrender.com:443',
+        auth: 'your_super_strong_password_here',
         secure: true
     }
 ];
